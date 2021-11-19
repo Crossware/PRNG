0a86d87eb01a45a09b42da86ef74ad9e8c182e58
Cosmin S <32804224+Crossware@users.noreply.github.com>
2021-11-19 17:04:33 +0000

    Populate dropdown values and info

--- src/Main.ts
+++ src/Main.ts
@@ -4,9 +4,14 @@ import MiddleSquare from './classes/MiddleSquare';
 var canvas = <HTMLCanvasElement> document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+let calculationTypes = new Array<CalculationType>(new LinearCongruential, new MiddleSquare);
+var selectedCalculation = (<HTMLSelectElement>document.getElementById("selectCalculation"));
+
+
 var y = 0;
 var congruential = new LinearCongruential();
-var currentIteration= 0;
+var currentIteration = 0;
+populateCalculationTypes();
 draw();
 function draw() {
   for(var x = 0; x < 600; x++) {
@@ -25,3 +30,26 @@ function draw() {
     requestAnimationFrame(draw);
   }
 }
+
+function populateCalculationTypes(){
+  var select = document.getElementById("selectCalculation");
+
+  for(var i = 0; i < calculationTypes.length; i++) {
+      var calculationType = calculationTypes[i];
+      var el = document.createElement("option");
+      el.textContent = calculationType.label;
+      el.nodeValue = calculationType.label;
+      select?.appendChild(el);
+  }
+}
+
+function populateCalculationTypeInfo(){
+  var head = document.getElementById("calculationTypeHead");
+  var body = document.getElementById("calculationTypeBody");
+  head!.innerHTML = selectedCalculation!.value;
+  body!.innerHTML = calculationTypes.find(e => e.label === selectedCalculation!.value)?.explanation!;
+}
+
+selectedCalculation.addEventListener('change', function(){
+  populateCalculationTypeInfo();
+});
